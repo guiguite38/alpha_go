b0f4fab916bb00691fa68a281fabb5707914cd0b
Mahaut <mmahaut@ensc.fr>
2020-11-20 11:50:15 +0100

    prior input/output in progress

--- Annexes/ml-go.py
+++ Annexes/ml-go.py
@@ -8,8 +8,9 @@ import tensorflow.keras.optimizers as optimizers
 
 ## LECTURE DES DONNEES
 
+
 def get_raw_data_go():
-    ''' Returns the set of samples from the local file or download it if it does not exists'''
+    """ Returns the set of samples from the local file or download it if it does not exists"""
     import gzip, os.path
     import json
 
@@ -17,8 +18,12 @@ def get_raw_data_go():
 
     if not os.path.isfile(raw_samples_file):
         print("File", raw_samples_file, "not found, I am downloading it...", end="")
-        import urllib.request 
-        urllib.request.urlretrieve ("https://www.labri.fr/perso/lsimon/ia-inge2/samples-9x9.json.gz", "samples-9x9.json.gz")
+        import urllib.request
+
+        urllib.request.urlretrieve(
+            "https://www.labri.fr/perso/lsimon/ia-inge2/samples-9x9.json.gz",
+            "samples-9x9.json.gz",
+        )
         print(" Done")
 
     with gzip.open("samples-9x9.json.gz") as fz:
@@ -28,8 +33,9 @@ def get_raw_data_go():
 
 ## COMPREHENSION DES DONNEES
 
+
 def summary_of_example(data, sample_nb):
-    ''' Gives you some insights about a sample number'''
+    """ Gives you some insights about a sample number"""
     sample = data[sample_nb]
     print("Sample", sample_nb)
     print()
@@ -37,34 +43,60 @@ def summary_of_example(data, sample_nb):
     print()
     print("The sample was obtained after", sample["depth"], "moves")
     print("The successive moves were", sample["list_of_moves"])
-    print("After these moves and all the captures, there was black stones at the following position", sample["black_stones"])
-    print("After these moves and all the captures, there was white stones at the following position", sample["white_stones"])
-    print("Number of rollouts (gnugo games played against itself from this position):", sample["rollouts"])
-    print("Over these", sample["rollouts"], "games, black won", sample["black_wins"], "times with", sample["black_points"], "total points over all this winning games")
-    print("Over these", sample["rollouts"], "games, white won", sample["white_wins"], "times with", sample["white_points"], "total points over all this winning games")
+    print(
+        "After these moves and all the captures, there was black stones at the following position",
+        sample["black_stones"],
+    )
+    print(
+        "After these moves and all the captures, there was white stones at the following position",
+        sample["white_stones"],
+    )
+    print(
+        "Number of rollouts (gnugo games played against itself from this position):",
+        sample["rollouts"],
+    )
+    print(
+        "Over these",
+        sample["rollouts"],
+        "games, black won",
+        sample["black_wins"],
+        "times with",
+        sample["black_points"],
+        "total points over all this winning games",
+    )
+    print(
+        "Over these",
+        sample["rollouts"],
+        "games, white won",
+        sample["white_wins"],
+        "times with",
+        sample["white_points"],
+        "total points over all this winning games",
+    )
 
 
 ## DONNEES EN ENTREE ET SORTIE DU MODELE
 
+
 def position_predict(black_stones, white_stones):
-    
+
     # ... Votre tambouille interne pour placer les pierres comme il faut dans votre structure de données
     # et appeler votre modèle Keras (typiquement avec model.predict())
-    prediction = None # model.predict(...) # A REMPLIR CORRECTEMENT
-    
+    prediction = None  # model.predict(...) # A REMPLIR CORRECTEMENT
+
     return prediction
 
 
-
-# Ainsi, pour le rendu, en admettant que newdata soit le fichier json contenant les nouvelles données que 
+# Ainsi, pour le rendu, en admettant que newdata soit le fichier json contenant les nouvelles données que
 # l'on vous donnera 24h avant la fin, vous pourrez construire le fichier resultat ainsi
 
+
 def create_result_file(newdata):
-    ''' Exemple de méthode permettant de générer le fichier de resultats demandés. '''
-    resultat  = [position_predict(d["black_stones"], d["white_stones"]) for d in newdata]
+    """ Exemple de méthode permettant de générer le fichier de resultats demandés. """
+    resultat = [position_predict(d["black_stones"], d["white_stones"]) for d in newdata]
     with open("my_predictions.txt", "w") as f:
-         for p in resultat:
-            f.write(str(p)+"\n")
+        for p in resultat:
+            f.write(str(p) + "\n")
 
 
 ######################################################
@@ -72,41 +104,73 @@ def create_result_file(newdata):
 
 # First steps: transform all the data into numpy arrays to feed your neural network
 
+
 def name_to_coord(s):
+    """
+    Prends en entrée une unique coord sous forme de chaine de caratères A2
+    Retourne une paire d'entiers représentant la coordonnée
+    """
     assert s != "PASS"
-    indexLetters = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'J':8}
+    indexLetters = {
+        "A": 0,
+        "B": 1,
+        "C": 2,
+        "D": 3,
+        "E": 4,
+        "F": 5,
+        "G": 6,
+        "H": 7,
+        "J": 8,
+    }
 
     col = indexLetters[s[0]]
     lin = int(s[1:]) - 1
-    return col, lin    
+    return [col, lin]
+
 
 def make_board(sample):
-    board = np.zeros((9,9,2), dtype=bool)
+    """
+    Creates representation of go board
+    2 grids -> 1 for white stones, 1 for black stones = (9,9,2) matrix
+    """
+    board = np.zeros((9, 9, 2), dtype=bool)
+
+    # print(sample["white_stones"])
+    # print(sample["black_stones"])
+
     for white_stone in sample["white_stones"]:
-        board[name_to_coord(white_stone),0] = True
+        x, y = name_to_coord(white_stone)
+        board[x, y, 0] = True
+
     for black_stone in sample["black_stones"]:
-        board[name_to_coord(black_stone),1] = True    
+        x, y = name_to_coord(black_stone)
+        board[x, y, 1] = True
     return board
 
 
-# if name == "__main__":
-    # Test of board making
-data = get_raw_data_go()
-#rval = np.random.randint(0,)
-board = make_board(data[10])
-print(board.shape)
-print(board[1,6,0])
-       
-    
-    # print("We have", len(data),"examples")
-    # # summary_of_example(data,10)
-    # # Par exemple, nous pourrons appeler votre prédiction ainsi
+# white stones at the following position ['C8', 'E7', 'H7', 'G3']
+# black stones at the following position ['B7', 'C6', 'E5', 'C3']
 
-    # print("Prediction this sample:")
-    # summary_of_example(data, 10)
-    # print()
-    # prediction = position_predict(data[10]["black_stones"], data[10]["white_stones"])
-    # print("You predicted", prediction, "and the actual target was", data[10]["black_wins"]/data[10]["rollouts"])
+
+# if name == "__main__":
+# Test of board making
+# rval = np.random.randint(0,)
+# board = make_board(data[10])
+# print(board.shape)
+# print(board[1, 6, 1])
+
+# print(board.T[0])
+# print(board.T[1])
+
+# print("We have", len(data),"examples")
+# # summary_of_example(data,10)
+# # Par exemple, nous pourrons appeler votre prédiction ainsi
+
+# print("Prediction this sample:")
+# summary_of_example(data, 10)
+# print()
+# prediction = position_predict(data[10]["black_stones"], data[10]["white_stones"])
+# print("You predicted", prediction, "and the actual target was", data[10]["black_wins"]/data[10]["rollouts"])
 
 
 ######################################################
@@ -114,17 +178,68 @@ print(board[1,6,0])
 
 # Second steps: build your neural network and train it
 
-#!! RECOMMENDED :
-#!! test Dense and Conv2D (!! downsize board !!) layers
-#!! 33% 'splitting rule' -> usually 80/20 !
+# on fait le rollout
+# pour l'améliorer on veut jouer des coups probables pour accélérer le roll out
+# donc on veut la proba de jouer un coup à partir d'un plateau donné
 
-#!! import tensorflow.keras
-#!! from tensorflow.keras.models import Sequential
-#!! from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-#!! from tensorflow.keras.layers import Conv2D, BatchNormalization
-#!! import tensorflow.keras.optimizers as optimizers
 
-# model = Sequential()
+# on veut make un board avec extraction préalable du dernier qui sera considéré comme un label
+def create_dataset_prior(data):
+    """
+    Takes list of moves as an input and returns [data,label]
+    with label = last move 
+    """
+    input_data = []
+    output_data = []
+    for sample in data:
+        output_sample = sample["list_of_moves"][3]
+        input_sample = {}
+
+        if len(sample["black_stones"]) > len(sample["white_stones"]):
+            input_sample["black_stones"] = sample["black_stones"][:-1]
+            input_sample["white_stones"] = sample["white_stones"]
+        else:
+            input_sample["black_stones"] = sample["black_stones"]
+            input_sample["white_stones"] = sample["white_stones"][:-1]
+
+        output_data.append(output_sample)
+        input_data.append(make_board(input_sample))
+    return input_data, output_data
+
+
+data = get_raw_data_go()[0]
+input_test, output_test = create_dataset_prior(data)
+
+print(output_test)
+
+data = get_raw_data_go()
+# (9,9,2) -> conv2D -> conv2D -> sigmoid(Dense) -> (9,9,2)
+
+model = Sequential()
+model.add(
+    Conv2D(filters=8, kernel_size=(3, 3), activation="relu", input_shape=(9, 9, 2))
+)
+# model.add(
+#     Conv2D(filters=8,kernel_size=(3,3), activation='relu')
+#     )
+# model.add(Dense(256))
+# model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(162, activation="softmax"))
+model.add(Dense(18, activation="softmax"))
+
+# label = mettre un B et un dans une 18*[0]
+
+# output -> (18,)
+# B6 -> B + 6
+# B : [0,1,2,3,] -> label [0,1,0,0,0,0,0,0,0,0,]
+# 6 -> [distribution] -> label = [0,0,0,0,0,1,0,0,0]
+# [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
+
+# print(model.summary())
+
+
+# model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=["accuracy"])
 
 # # ... A REMPLIR
 
--- myPlayer.py
+++ myPlayer.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
-''' This is the file you have to modify for the tournament. Your default AI player must be called by this module, in the
+""" This is the file you have to modify for the tournament. Your default AI player must be called by this module, in the
 myPlayer class.
 
 Right now, this class contains the copy of the randomPlayer. But you have to change this!
-'''
+"""
 
 import time
-import Goban 
+import Goban
 from random import choice
 from playerInterface import *
 
+
 class myPlayer(PlayerInterface):
-    ''' Example of a random player for the go. The only tricky part is to be able to handle
+    """ Example of a random player for the go. The only tricky part is to be able to handle
     the internal representation of moves given by legal_moves() and used by push() and 
     to translate them to the GO-move strings "A1", ..., "J8", "PASS". Easy!
-    '''
+    """
 
     def __init__(self):
         self._board = Goban.Board()
-        self._mycolor = None        
-        score= self._board.Goban.compute_score() # !! slow, to be replaced --> faire un lazyloading en attendant
-        self._heuristic = scores[abs(self._mycolor)] - scores[abs(self._mycolor-1)]
+        self._mycolor = None
+        score = (
+            self._board.Goban.compute_score()
+        )  # !! slow, to be replaced --> faire un lazyloading en attendant
+        self._heuristic = scores[abs(self._mycolor)] - scores[abs(self._mycolor - 1)]
 
     def getPlayerName(self):
         return "Random Player"
@@ -28,20 +31,20 @@ class myPlayer(PlayerInterface):
     def getPlayerMove(self):
         if self._board.is_game_over():
             print("Referee told me to play but the game is over!")
-            return "PASS" 
-        moves = self._board.legal_moves() # Dont use weak_legal_moves() here!
-        
+            return "PASS"
+        moves = self._board.legal_moves()  # Dont use weak_legal_moves() here!
+
+        # TODO
         # remplacer par le maximum de w/n + c *np.sqrt(ln(N)/n)
         # w = nb wins by successor
         # n = nb visits of successor
-        # c = sqrt(2) 
+        # c = sqrt(2)
         # N = nb visits of father node
-        
 
         # prior --> Rollout
         # value --> scoring system
-            
-        move = choice(moves) # random choice
+
+        move = choice(moves)  # random choice
         self._board.push(move)
 
         # New here: allows to consider internal representations of moves
@@ -49,12 +52,12 @@ class myPlayer(PlayerInterface):
         print("My current board :")
         self._board.prettyPrint()
         # move is an internal representation. To communicate with the interface I need to change if to a string
-        return Goban.Board.flat_to_name(move) 
+        return Goban.Board.flat_to_name(move)
 
     def playOpponentMove(self, move):
-        print("Opponent played ", move) # New here
-        # the board needs an internal represetation to push the move.  Not a string
-        self._board.push(Goban.Board.name_to_flat(move)) 
+        print("Opponent played ", move)  # New here
+        #  the board needs an internal represetation to push the move.  Not a string
+        self._board.push(Goban.Board.name_to_flat(move))
 
     def newGame(self, color):
         self._mycolor = color
@@ -65,6 +68,4 @@ class myPlayer(PlayerInterface):
             print("I won!!!")
         else:
             print("I lost :(!!")
-
-
 
